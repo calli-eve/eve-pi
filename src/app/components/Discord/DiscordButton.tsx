7d8c6227111d3bb14e7501729e477315e0376be4
Calli <calli-eve@proton.me>
2024-04-27 22:10:29 +0300

    Update discord invite link

--- src/app/components/Discord/DiscordButton.tsx
+++ src/app/components/Discord/DiscordButton.tsx
@@ -4,7 +4,7 @@ export const DiscordButton = () => {
     <Box>
       <Tooltip title="Come nerd out in discord about PI and this tool">
         <Button
-          href="https://discord.gg/Kk53c9fG"
+          href="https://discord.gg/GPtw5kfuJu"
           target="_blank"
           style={{ width: "100%" }}
           sx={{ color: "white", display: "block" }}

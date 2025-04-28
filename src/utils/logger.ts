93507ea98eb4352a88f5b12f2557fd753e1af45d
calli <calli-eve@proton.me>
2025-04-28 18:27:35 +0300

    add pino logger configuration correctly

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,14 +2,12 @@ import pino from 'pino';
 
 const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname',
+  formatters: {
+    level: (label) => {
+      return { level: label };
     },
   },
+  timestamp: () => `,"time":"${new Date().toISOString()}"`,
   base: {
     env: process.env.NODE_ENV,
   },
